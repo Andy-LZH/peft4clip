assign backprop to correct parameter

src/model/vpt_clip/vpt_clip.py
@@ -54,9 +54,13 @@ class CLIPEmbedding(Embeddings):
         width = x.shape[1]
         scale = width**-0.5
         patch_size = x.shape[2]
-        self.class_embedding = nn.Parameter(scale * torch.randn(width))
+        self.class_embedding = nn.Parameter(
+            scale * torch.randn(width), requires_grad=False
+        )
         self.positional_embedding = nn.Parameter(
-            scale * torch.randn((self.input_resolution[0] // patch_size) ** 2 + 1, width)
+            scale
+            * torch.randn((self.input_resolution[0] // patch_size) ** 2 + 1, width),
+            requires_grad=False,
         )
 
         # reform embeddings
@@ -186,6 +190,48 @@ class VisionPromptCLIP(nn.Module):
         # check dim-1 n_prompt
         return embedding
 
+    def encode_image(self, image):
+        img = image
+        # incorporate prompt
+        incoporated_prompt = self.incorporate_prompt(
+            img
+        )  # (batch_size, 1 + n_patches + n_prompt, hidden_dim)
+
+        # reform embedding to match CLIP
+        incoporated_prompt = incoporated_prompt.permute(
+            1, 0, 2
+        )  # (1 + n_patches + n_prompt, batch_size, hidden_dim)
+
+        # make sure self.ViT.transformer not update but incoporated_prompt can
+        input_embedding = self.ViT.transformer(incoporated_prompt)
+
+        # reform embedding to perform loss calculation
+        input_embedding = input_embedding.permute(
+            1, 0, 2
+        )  # (batch_size, 1 + n_patches + n_prompt, hidden_dim)
+        # Layernorm
+
+        input_embedding = self.ViT.ln_post(
+            input_embedding[:, 0, :]
+        )  # (batch_size, hidden_dim)
+
+        # project to output dim
+        if self.ViT.output_dim != input_embedding.shape[-1]:
+            input_embedding = (
+                input_embedding @ self.ViT.proj
+            )  # (batch_size, output_dim)
+
+        image_features_vpt = input_embedding
+
+        text_features = self.model.encode_text(self.prompts)
+
+        # after calculating text features, convert back to fp32
+        image_features_vpt = image_features_vpt / image_features_vpt.norm(
+            dim=-1, keepdim=True
+        )
+
+        return image_features_vpt
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of Vision Prompt CLIP
@@ -200,46 +246,20 @@ class VisionPromptCLIP(nn.Module):
         logits : torch.Tensor
             Output logits tensor
         """
-        # enable autocast for mixed precision
-        img = x
+        image_features_vpt = self.encode_image(x)
 
-        # incorporate prompt
-        incoporated_prompt = self.incorporate_prompt(img) # (batch_size, 1 + n_patches + n_prompt, hidden_dim)
-
-        # reform embedding to match CLIP
-        incoporated_prompt = incoporated_prompt.permute(1, 0, 2)  # (1 + n_patches + n_prompt, batch_size, hidden_dim)
-
-        # Frozen CLIP forward pass
-        with torch.no_grad():
-            transformer = self.ViT.transformer
-            input_embedding = transformer(incoporated_prompt)
-            
-            # reform embedding to perform loss calculation
-            input_embedding = input_embedding.permute(1, 0, 2)  # (batch_size, 1 + n_patches + n_prompt, hidden_dim)
-            # Layernorm
-            input_embedding = self.ViT.ln_post(
-                input_embedding[:, 0, :]
-            )  # (batch_size, hidden_dim)
-
-            # project to output dim
-            if self.ViT.output_dim != input_embedding.shape[-1]:
-                input_embedding = input_embedding @ self.ViT.proj  # (batch_size, output_dim)
-
-        image_features_vpt = input_embedding
-
-        # Freeze CLIP forward pass
         with torch.no_grad():
             text_features = self.model.encode_text(self.prompts)
-
-        # after calculating text features, convert back to fp32
-        image_features_vpt = image_features_vpt / image_features_vpt.norm(
-            dim=-1, keepdim=True
-        )
         text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
         logit_scale = self.model.logit_scale
         logits = logit_scale * (image_features_vpt @ text_features.t())
+        self.prompt_proj.train()
+        self.prompt_dropout.train()
         return logits
+    
+    def linear_probe(self, x: torch.Tensor) -> torch.Tensor:
+        image_features_vpt = self.encode_image(x)
 
     def train(self):
         """
@@ -248,10 +268,13 @@ class VisionPromptCLIP(nn.Module):
 
         # freeze CLIP
         self.model.eval()
+        self.ViT.eval()
+        self.ViT.transformer.eval()
+        self.embeddings.eval()
 
         # set model to train mode
-        self.prompt_proj.eval()
-        self.prompt_dropout.eval()
+        self.prompt_proj.train()
+        self.prompt_dropout.train()
 
     def eval(self):
         """

train.py
@@ -75,9 +75,16 @@ def main():
     predicted = []
     labels = []
 
-    # define optimizer and loss function
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-    loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
+    # define optimizer and loss function, update only two parameters (prompt_dropout and prompt_proj)
+    optimizer = torch.optim.SGD(
+        [
+            {"params": model.prompt_dropout.parameters()},
+            {"params": model.prompt_proj.parameters()},
+        ],
+        lr=dataset_config.SOLVER.BASE_LR,
+        weight_decay=dataset_config.SOLVER.WEIGHT_DECAY,
+    )
+    loss_fn = torch.nn.CrossEntropyLoss()
 
     pbar = tqdm(train_loader)
     torch.autograd.set_detect_anomaly(True)
@@ -87,6 +94,11 @@ def main():
         for img, label, idx in pbar:
             # TODO check how this improve linear probe accuracy
             # dynamically cast to fp16 to save memory and comatible with clip
+
+            # check if parameters has been updated
+            for name, param in model.named_parameters():
+                if param.requires_grad:
+                    print(name, param.data.sum())
 
             # forward pass
             with autocast():
@@ -107,6 +119,9 @@ def main():
             _, indices = logits.max(1)
             predicted.append(indices.cpu().numpy())
             labels.append(label.cpu().numpy())
+
+            print("Predicted: ", list(indices.cpu().numpy()))
+            print("Labels: ", list(label.cpu().numpy()))
 
             accuracy = (indices == label.to(args.device)).float().mean().item()
 